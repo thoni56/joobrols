5c167ecab031968c346fc985ab07a53f98b743e0
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2020-01-09 16:10:44 +0100

    Mark scraped before recursing (duh)

--- joobrols.py
+++ joobrols.py
@@ -38,25 +38,33 @@ def scrape_page(site, path):
     all_links.append(path)
     if all_links.get(path).scraped:
         return
+    else:
+        all_links.get(path).scraped = True
+    print("Scraping", path)
     url = site+path
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
     # Check for broken link
     results = soup.find(id='content-area')
-    if len(results.contents) == 0:
+    if not results or len(results.contents) == 0:
         # TODO: mark path as broken
         print(path, " is empty")
     
     discovered_links = soup.find_all('a')
     for l in discovered_links:
         discovered_url = l.get('href')
-        if is_internal_link(discovered_url):
+        if discovered_url and is_relevant_link(discovered_url):
             scrape_page(site, discovered_url)
-    all_links.get(path).scraped = True
 
-def is_internal_link(discovered_url):
-    return not discovered_url.startswith("http") and not discovered_url.startswith("#") and not discovered_url.startswith("javascript")
+def is_relevant_link(discovered_url):
+    return not discovered_url.startswith("http") \
+        and not discovered_url.startswith("#") \
+        and not discovered_url.startswith("javascript") \
+        and not discovered_url.endswith(".pdf") \
+        and not discovered_url.endswith(".jpg") \
+        and not discovered_url.endswith("print=1") \
+        and not "component/mailto" in discovered_url
         
 if (__name__ == "__main__"):
 
@@ -73,3 +81,4 @@ if (__name__ == "__main__"):
     all_links = Links(site)
 
     scrape_page(site, parsed_url.path)
+    print("Scraped", all_links.length(), "paths")
