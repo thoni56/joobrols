dac06354f44296bc0f06ef55f181619ebef1acff
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2020-01-10 11:52:44 +0100

    Lots of changes

--- joobrols.py
+++ joobrols.py
@@ -1,4 +1,4 @@
-#! /bin/env python3
+#! /usr/bin/env python3
 
 import requests
 from bs4 import BeautifulSoup
@@ -38,47 +38,94 @@ class Links:
 
 
 all_links = None
+max_links = 0
+verbose = False
+indent = 0
 
 def scrape_page(site, path):
-    all_links.append(path)
-    if all_links.get(path).scraped:
+    global indent
+    if max_links != 0 and len(all_links.links) > max_links:
+        if verbose:
+            print("***", "  "*indent, "Ignored because of --max", path)
+        return
+
+    link = all_links.append(path)
+    if link.scraped:
+        if verbose:
+            print("***", "  "*indent, "Already scraped", path)
         return
     else:
-        all_links.get(path).scraped = True
-    print(".", end="")
+        link.scraped = True
+
     url = site+path
+    if not verbose:
+        print(".", end="", flush=True)
+
+    if not internal_link(path):
+        if verbose:
+            print("***", "  "*indent, "Reading external link", path)
+        try:
+            page = requests.get(path, timeout=10)
+            if page.status_code != 200:
+                link.broken = True
+        except requests.exceptions.Timeout:
+            link.broken = True
+        return
+
+    if verbose:
+        print("***", "  "*indent, "Reading internal link", path)
     page = requests.get(url)
+
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    # Check for broken link
+    # Check for link to empty page in Joomla
     results = soup.find(id='content-area')
     if not results or len(results.contents) == 0:
-        all_links.get(path).broken = True
+        link.broken = True
     
-    discovered_links = soup.find_all('a')
-    for l in discovered_links:
-        discovered_url = l.get('href')
-        if discovered_url and is_relevant_link(discovered_url):
-            scrape_page(site, discovered_url)
-        # TODO Distinguish between pages to be scraped and links that just need checking for existance
+    if internal_link(link.path):
+        discovered_links = soup.find_all('a')
+        indent = indent + 1
+        for l in discovered_links:
+            discovered_url = l.get('href')
+            if discovered_url and is_relevant_link(discovered_url):
+                if verbose:
+                    print("***", "  "*indent, "Discovered", discovered_url)
+                scrape_page(site, discovered_url)
+                link = all_links.get(discovered_url)
+                if link:
+                    # Might be ignored because of --max
+                    link.sources.append(path)
+        indent = indent - 1
 
 def is_relevant_link(discovered_url):
-    return not discovered_url.startswith("http") \
-        and not discovered_url.startswith("#") \
+    return not discovered_url.startswith("#") \
         and not discovered_url.startswith("javascript") \
         and not discovered_url.endswith(".pdf") \
         and not discovered_url.endswith(".jpg") \
         and not discovered_url.endswith("print=1") \
+        and not discovered_url.endswith("print=1&layout=default") \
         and not "component/mailto" in discovered_url
+
+def internal_link(discovered_url):
+    return not discovered_url.startswith("http")
         
 if (__name__ == "__main__"):
 
     argparser = argparse.ArgumentParser(
-        description='Explore all internal links in a Joomla site and find which ones point to empty "pages"')
+        description='Explore all links in a Joomla site and find which ones are broken or point to empty "pages"')
     argparser.add_argument(
         'url', help='URL to site')
+    argparser.add_argument('--max', '-m', type=int, help='Maximum number of links to accumulate before analyzing')
+    argparser.add_argument('--verbose', '-v', action='store_true')
 
     args = argparser.parse_args()
+
+    if args.max:
+        max_links = args.max
+    
+    if args.verbose:
+        verbose = True
 
     parsed_url = urlparse(args.url)
     site = parsed_url.scheme+'://'+parsed_url.netloc
@@ -86,9 +133,11 @@ if (__name__ == "__main__"):
     all_links = Links(site)
 
     scrape_page(site, parsed_url.path)
-    print("Scraped", all_links.length(), "paths")
+    print("\nScraped", all_links.length(), "paths")
 
-    print("Broken paths:")
-    for link in all_links.links:
-        if link.broken:
-            print("\t", link.path)
+    broken_links = list(filter(lambda l: l.broken, all_links.links))
+    print(len(broken_links), "broken paths found:")
+    for link in broken_links:
+        print("\t", link.path, "found in")
+        for source in link.sources:
+            print("\t\t", site+source)
